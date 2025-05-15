76fec9af2ae35a9b7baebb07f7b688a0d648e1d8
atusoliverss <129017670+atusoliverss@users.noreply.github.com>
2025-05-15 10:08:53 -0300

    Estrutura Base do Projeto

---
+++ src/main/java/br/com/prodsys/ProdSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.prodsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prodsys/ProdSysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prodsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
